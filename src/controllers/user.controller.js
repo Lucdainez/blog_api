eff09e1be9b50c6e553dd23ca238c719a655c49c
Lucas Dainez <lucdainez@gmail.com>
2022-12-02 14:32:55 -0300

    feat(user.controller.js): adicionar requisição de usuário por ID

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -14,7 +14,15 @@ const getAllUsers = async (req, res) => {
   return res.status(200).json(users);
 };
 
+const getUser = async (req, res) => {
+  const { id } = req.params;
+  const { type, message } = await userService.getUserById(id);
+  if (type) return res.status(type).json({ message });
+  return res.status(200).json(message);
+};
+
 module.exports = {
   insertUser,
   getAllUsers,
+  getUser,
 };
