2eafe2951b7946093a6a21846c65a9312c56f804
Lucas Dainez <lucdainez@gmail.com>
2022-12-02 13:59:11 -0300

    feat(jwtFunctions.js): adicionar função de verificação do token

--- src/auth/jwtFunctions.js
+++ src/auth/jwtFunctions.js
@@ -10,6 +10,16 @@ const jwtConfig = {
 const createToken = (userWithoutPassword) => jwt
   .sign({ data: userWithoutPassword }, secret, jwtConfig);
 
+const verifyToken = (authorization) => {
+  try {
+    const payload = jwt.verify(authorization, secret);
+    return { type: null, message: payload };
+  } catch (error) {
+    return { type: 401, message: 'Expired or invalid token' };
+  }
+};
+
 module.exports = {
   createToken,
+  verifyToken,
 };
