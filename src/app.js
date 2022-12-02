326b2ff1d39571f7c237438cc62684313fea8d8f
Lucas Dainez <lucdainez@gmail.com>
2022-12-02 00:09:33 -0300

    feat(app): adicionar rota '/user'

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const loginRouter = require('./routers/login.router');
 
+const userRouter = require('./routers/user.router');
+
 // ...
 
 const app = express();
@@ -9,6 +11,8 @@ const app = express();
 app.use(express.json());
 
 app.use('/login', loginRouter);
+
+app.use('/user', userRouter);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
