ee69b4abaec8a87d489287ddfaff2eb4b12b2e75
Lucas Dainez <lucdainez@gmail.com>
2022-12-02 00:16:32 -0300

    feat(passwordValidation.middleware.js): adicionar validação  do password
    é verificado se ele contém no mínimo 6 caracteres

---
+++ src/middlewares/passwordValidation.middleware.js
@@ -0,0 +1,9 @@
+module.exports = (req, res, next) => {
+  const { password } = req.body;
+  if (password.length < 6) {
+    return res.status(400).json({
+      message: '"password" length must be at least 6 characters long',
+    });
+  }
+  return next();
+};
