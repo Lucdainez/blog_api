df95b574892e9ae8816cbe723d1902da46625dc4
Lucas Dainez <lucdainez@gmail.com>
2022-12-01 21:02:53 -0300

    feat(app): adicionar rota
    foi adicionado a rota '/login'

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
+const loginRouter = require('./routers/login.router');
+
 // ...
 
 const app = express();
 
 app.use(express.json());
 
-// ...
+app.use('/login', loginRouter);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
