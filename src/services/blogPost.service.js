f7510068eefd97738fcb136f0f8f5e3ff2da2e2c
Lucas Dainez <lucdainez@gmail.com>
2022-12-03 13:58:08 -0300

    feat(blogPost.service.js): adicionar regra de negócio para puxar um post pelo ID

--- src/services/blogPost.service.js
+++ src/services/blogPost.service.js
@@ -10,6 +10,22 @@ const getPosts = async () => {
   return posts;
 };
 
+const getPostId = async (id) => {
+  const postId = await BlogPost.findOne({
+    where: { id },
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } }, 
+      { model: Category, as: 'categories', through: { attributes: [] } },
+    ],
+  });
+
+  if (!postId) {
+    return { type: 404, message: 'Post does not exist' };
+  }
+  return { type: null, message: postId };
+};
+
 module.exports = {
   getPosts,
+  getPostId,
 };
