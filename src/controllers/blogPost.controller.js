dd06f61e181d09ae42c190e94a094c421c06eca6
Lucas Dainez <lucdainez@gmail.com>
2022-12-03 17:10:25 -0300

    feat(blogPost.controller.js): adicionar requisição do tipo PUT

--- src/controllers/blogPost.controller.js
+++ src/controllers/blogPost.controller.js
@@ -1,5 +1,7 @@
 const blogPostService = require('../services/blogPost.service');
 
+const jwtFunctions = require('../auth/jwtFunctions');
+
 const getPosts = async (_req, res) => {
   const value = await blogPostService.getPosts();
   return res.status(200).json(value);
@@ -14,7 +16,20 @@ const getPostId = async (req, res) => {
   return res.status(200).json(message);
 };
 
+const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const { authorization } = req.headers;
+  const { title, content } = req.body;
+  const userId = jwtFunctions.decodeTokenGetIdUser(authorization);
+  const { type, message } = await blogPostService.updatePost(id, userId, title, content);
+  if (type) {
+    return res.status(type).json({ message });
+  }  
+  return res.status(200).json(message);
+};
+
 module.exports = {
   getPosts,
   getPostId,
+  updatePost,
 };
