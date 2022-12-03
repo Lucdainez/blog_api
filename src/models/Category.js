20ea05437d7a1a630d33272944fbfd2d1fce9954
Lucas Dainez <lucdainez@gmail.com>
2022-12-03 12:47:57 -0300

    feat(Category.js): adicionar propriedade tableName

--- src/models/Category.js
+++ src/models/Category.js
@@ -13,6 +13,7 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     timestamps: false,
     underscored: true,
+    tableName: 'categories',
     // modelName: 'category',
   });
   return category;
