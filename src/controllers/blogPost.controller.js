fb9d4197aaa89f8d2f8abae634a18228288dcce3
Lucas Dainez <lucdainez@gmail.com>
2022-12-04 18:53:43 -0300

    feat(blogPost.controller.js): adicionar duas requisições dos tipos DELETE e POST

--- src/controllers/blogPost.controller.js
+++ src/controllers/blogPost.controller.js
@@ -28,8 +28,38 @@ const updatePost = async (req, res) => {
   return res.status(200).json(message);
 };
 
+const deletePost = async (req, res) => {
+  const { id } = req.params;
+  const { authorization } = req.headers;
+  const userId = jwtFunctions.decodeTokenGetIdUser(authorization);
+  const postId = await blogPostService.getPostId(id);
+  if (postId.type) {
+    return res.status(postId.type).json({ message: postId.message });
+  }
+  const { type, message } = await blogPostService.deletePost(id, userId);
+  if (type) {
+    return res.status(type).json({ message });
+  }
+  return res.status(204).json(userId);
+};
+
+const insertPost = async (req, res) => {
+  const { title, content, categoryIds } = req.body;
+  const { authorization } = req.headers;
+  const userId = jwtFunctions.decodeTokenGetIdUser(authorization);
+  console.log(userId);
+  const { type, message } = await blogPostService
+    .insertPost({ title, content, categoryIds, userId });
+  if (type) {
+    return res.status(type).json({ message });
+  }
+  return res.status(201).json(message);
+};
+
 module.exports = {
   getPosts,
   getPostId,
   updatePost,
+  deletePost,
+  insertPost,
 };
