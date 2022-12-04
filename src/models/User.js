a2e0c49c3a6a33e759eb7e18631d3b28d74b6f58
Lucas Dainez <lucdainez@gmail.com>
2022-12-04 18:56:42 -0300

    fix(User.js): mudar foreignKey de sneak_case para camelCase

--- src/models/User.js
+++ src/models/User.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
 
   user.associate = (models) => {
     user.hasMany(models.BlogPost, {
-      foreignKey: 'user_id',
+      foreignKey: 'userId',
       as: 'blogPost',
     });
   };
